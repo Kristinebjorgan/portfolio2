c7561b0820188784c095f76f43b27d1cb3841401
Kristinebjorgan <kribjo53662@stud.noroff.no>
2025-06-02 19:42:48 +0100

    Set up Tailwind with custom config and basic page routing

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        greenish: "#296f5c",
+        gold: "#c2a14d",
+        offwhite: "#f8f4ec",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
